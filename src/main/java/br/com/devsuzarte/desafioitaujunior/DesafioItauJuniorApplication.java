f266209d1727f6b1dc69b26a5001f36943aa224e
BenSuzarte <ben.dantassuzarte@gmail.com>
2024-10-14 15:23:32 -0300

    Initial commit.

---
+++ src/main/java/br/com/devsuzarte/desafioitaujunior/DesafioItauJuniorApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuzarte.desafioitaujunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devsuzarte/desafioitaujunior/DesafioItauJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuzarte.desafioitaujunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
