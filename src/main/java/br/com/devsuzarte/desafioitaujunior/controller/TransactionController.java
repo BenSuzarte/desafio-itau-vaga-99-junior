03834ce30f8ad3f21f4805f441df6d85a7a18b0c
BenSuzarte <ben.dantassuzarte@gmail.com>
2024-10-23 13:30:33 -0300

    feat: combine controller and service for transaction processing

--- src/main/java/br/com/devsuzarte/desafioitaujunior/controller/TransactionController.java
+++ src/main/java/br/com/devsuzarte/desafioitaujunior/controller/TransactionController.java
@@ -1,14 +1,18 @@
 package br.com.devsuzarte.desafioitaujunior.controller;
 
 import br.com.devsuzarte.desafioitaujunior.entity.TransactionEntity;
+import br.com.devsuzarte.desafioitaujunior.exception.TransactionException.InvalidTransactionException;
 import br.com.devsuzarte.desafioitaujunior.service.TransactionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(
         value = "/transacao", // Nome obrigatório do endpoint
@@ -19,9 +23,25 @@ public class TransactionController {
 
     private TransactionService transactionService;
 
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
     // Responsável pela realização transação
+    @PostMapping()
     public ResponseEntity<Void> transfer(@RequestBody TransactionEntity transaction) {
-        return ResponseEntity.status(HttpStatus.CREATED).build(); // 201: CREATED
+        try {
+            log.info("Starting transfer...");
+            transactionService.perform(transaction);
+
+            return ResponseEntity.status(HttpStatus.CREATED).build(); // 201: CREATED
+
+        } catch (InvalidTransactionException err) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+
+        } catch (Exception err) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
 }
--- src/main/java/br/com/devsuzarte/desafioitaujunior/service/TransactionService.java
+++ src/main/java/br/com/devsuzarte/desafioitaujunior/service/TransactionService.java
@@ -1,22 +1,31 @@
 package br.com.devsuzarte.desafioitaujunior.service;
 
 import br.com.devsuzarte.desafioitaujunior.entity.TransactionEntity;
-import br.com.devsuzarte.desafioitaujunior.exception.TransactionException;
+import br.com.devsuzarte.desafioitaujunior.exception.TransactionException.InvalidTransactionException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Slf4j
 @Service
 public class TransactionService {
 
     public void perform(TransactionEntity transaction) {
+        log.info("Validating transaction");
         ValidateTransaction(transaction);
+
+        log.info("Finished validating transaction");
+        return;
     }
 
     private void ValidateTransaction(TransactionEntity transaction) {
-        if(transaction.getValor().compareTo(BigDecimal.ZERO) < 0) throw new TransactionException.InvalidTransactionException("Menor que 0");
-        if(transaction.getDataHora().isAfter(OffsetDateTime.now())) throw new TransactionException.InvalidTransactionException("Data incompatível");
+        log.info("Verifying value");
+        if(transaction.getValor().compareTo(BigDecimal.ZERO) < 0) throw new InvalidTransactionException("Invalid value");
+
+        log.info("Verifying dateTime");
+        if(transaction.getDataHora().isAfter(OffsetDateTime.now())) throw new InvalidTransactionException("Invalid dateTime");
     }
 
 }
